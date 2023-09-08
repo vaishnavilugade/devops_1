1f50f566a4b6ecc2d3a8a69d60bb6586b0a993a7
vaishnavi <vaishnavi@gmail.com>
2023-09-08 14:48:00 +0530

    changed class name

--- sub.java
+++ sub.java
@@ -1,8 +1,8 @@
-class sub{
+class Add{
   public static void main(String[] args){
     int a=10;
     int b=20;
-    int c=a-b;
-    System.out.println("substraction of a and b is: "+c);
+    int c=a+b;
+    System.out.println("add of a and b is: "+c);
   }
 }
